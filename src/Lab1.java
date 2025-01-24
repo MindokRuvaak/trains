83646af9b422798350c1e3d1e630abad70ae5a89
MindokRuvaak <cole_daniel@rockatmil.com>
2025-01-24 15:02:05 +0100

    initial trainHandler

--- src/Lab1.java
+++ src/Lab1.java
@@ -1,16 +1,38 @@
 import TSim.*;
+import java.util.concurrent.Semaphore;
+import java.lang.Math;
+
 
 public class Lab1 {
 
   public Lab1(int speed1, int speed2) {
     TSimInterface tsi = TSimInterface.getInstance();
 
-    try {
-      tsi.setSpeed(1,speed1);
+    trainHandler train1 = new trainHandler(tsi, 1, speed1);
+    trainHandler train2 = new trainHandler(tsi, 2, speed2);
+
+
+  }
+
+  private class trainHandler {
+    private static final int MAXSPEED = 30;
+    private final TSimInterface tsi;
+    private final int id;
+  
+
+    trainHandler(TSimInterface tsi, int id, int initSpeed) {
+      this.tsi = tsi;
+      this.id = id;
+      setSpeed(initSpeed);
     }
-    catch (CommandException e) {
-      e.printStackTrace();    // or only e.getMessage() for the error
-      System.exit(1);
+
+    void setSpeed(int speed) {
+      try {
+        this.tsi.setSpeed(this.id, Math.clamp(speed, -MAXSPEED, MAXSPEED) );
+      } catch (CommandException e) {
+        e.printStackTrace(); // or only e.getMessage() for the error
+        System.exit(1);
+      }
     }
   }
 }
