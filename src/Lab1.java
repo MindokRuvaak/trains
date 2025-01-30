9ee802b516647a80e2b1b0b65eb0861b1e2dcffd
ik-joong <chang.ikjoong@gmail.com>
2025-01-30 15:26:00 +0100

    dunno

--- src/Lab1.java
+++ src/Lab1.java
@@ -5,36 +5,77 @@ import java.lang.Math;
 
 public class Lab1 {
 
-  public Lab1(int speed1, int speed2) {
+  public Lab1(int speed1, int speed2) throws InterruptedException  {
     TSimInterface tsi = TSimInterface.getInstance();
 
     trainHandler train1 = new trainHandler(tsi, 1, speed1);
     trainHandler train2 = new trainHandler(tsi, 2, speed2);
+
+    Thread t1 =new Thread(train1);
+    Thread t2 =new Thread(train2);
+
+    t1.start();
+    t2.start();
     
     Semaphore[] sems = new Semaphore[9];
+    //sempahore 0:
+    //sempahore 1:
+    //sempahore 2:
+    //sempahore 3:
+    //sempahore 4:
+    //sempahore 5:
+    //sempahore 6:
+    //sempahore 7:
+    //sempahore 8:
 
   }
 
-  private class trainHandler {
+  class trainHandler implements Runnable{
     private static final int MAXSPEED = 30;
 
     private final TSimInterface tsi;
     private final int id;
+    private int speed;
+
+    private boolean criticalSection;
   
 
     trainHandler(TSimInterface tsi, int id, int initSpeed) {
       this.tsi = tsi;
       this.id = id;
+      this.speed = initSpeed;
+
+      this.criticalSection=false;
+
       setSpeed(initSpeed);
     }
 
     void setSpeed(int speed) {
       try {
-        this.tsi.setSpeed(this.id, Math.clamp(speed, -MAXSPEED, MAXSPEED) );
+        this.tsi.setSpeed(this.id, Math.clamp(speed, -MAXSPEED, MAXSPEED));
       } catch (CommandException e) {
         e.printStackTrace(); // or only e.getMessage() for the error
         System.exit(1);
       }
     }
+
+    @Override
+    public void run() {
+      try {
+        while (true) {
+          SensorEvent sensor = tsi.getSensor(id);
+          //sensorHandler(sensor);
+        }
+      } catch (CommandException | InterruptedException e) {
+        e.printStackTrace(); // or only e.getMessage() for the error
+      }
+    }
+
+    public void sensorHandler(SensorEvent sens, Semaphore sem){
+      int x = sens.getXpos();
+      int y = sens.getYpos();
+    }
+
+  
   }
 }
